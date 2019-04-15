facd4767f08b8e5141f7e1087f4a8fcb77420350
Piggilinux <37959674+Piggilinux@users.noreply.github.com>
2019-04-15 06:18:47 -0500

    Add files via upload

---
+++ insert_values.sql
@@ -0,0 +1,25 @@
+SELECT create_user('bob@mail.com', 'ThisAPasswordYoCheckItOutBro', 'Bob', 'Marley', '1337101');
+SELECT create_user('Robby@mail.com', 'password', 'Bobby', 'Snobb', '34733');
+SELECT create_user('janne@mail.com', 'ThisIsNotAGoodPwd', 'Janne', 'Svensson', 'lol');
+SELECT create_user('erik@mail.com', 'watDisIs?', 'erik', 'Svensson', '21229');
+
+SELECT create_user('hulk@mail.com', 'HulkSmash', 'Bruce', 'Banner', '');
+SELECT create_user('ironman@mail.com', 'Pepper<3', 'Tony', 'Stark', '101');
+SELECT create_user('lordofthunder@mail.com', 'DoINeedThis?', 'Thor', 'Son of Odin', '');
+SELECT create_user('spiderman@mail.com', '!Sup3rS3cr3t!', 'Peter', 'Parker', '291535');
+SELECT create_user('antman@mail.com', 'itsMrAntToYou', 'Scott', 'Lang', '477');
+SELECT create_user('blackpanter@mail.com', 'Wakanda_4ever', 'T', 'Challa', '21229');
+-- SELECT * FROM users INNER JOIN pwd ON users.usr_ID = pwd.FK_usr_ID;
+-- SELECT change_password(10, 'newpwd', 'Wakanda_4ever', 'Wakanda_4ever');
+
+CALL add_friend(1, 3); # Bob Marley(1) requests Janne Svensson(3)
+CALL add_friend(1, 4); # Bob Marley(1) requests Erik Svensson(3)
+CALL add_friend(5, 6); # Bruce Banner(1) requests Tony Stark(3)
+CALL add_friend(8, 6); # Peter Parker(1) requests Tony Stark(3)
+CALL add_friend(6, 10); # Tony Stark(1) requests T'Challa(3)
+-- SELECT * FROM friend_request;
+CALL accept_friend_request(3, 1); # Janne(3) accepts Bob(1)
+CALL accept_friend_request(6, 5); # Tony Stark(6) requests Bruce Banner(5)
+CALL accept_friend_request(6, 8); # Tony Stark(6) requests Peter Parker(8) 
+CALL accept_friend_request(10, 6); #  T'Challa(10) requests Tony Stark(6)
+-- SELECT * FROM usr_relations;
--- procedures.sql
+++ procedures.sql
@@ -107,3 +107,31 @@ CREATE PROCEDURE accept_friend_request (whoAmI VARCHAR(255), toAdd VARCHAR(255))
   END //
 DELIMITER ;
 
+
+
+# Returns 1 if pwd changed, 2 if old password was not correct and 3 if new passwordcheck fails.
+DROP FUNCTION IF EXISTS change_password;
+DELIMITER //
+CREATE FUNCTION change_password (whoAmI VARCHAR(255), old_pwd VARCHAR(255), new_pwd VARCHAR(255), new_pwd_check VARCHAR(255))
+RETURNS INT
+NOT DETERMINISTIC
+BEGIN
+	DECLARE return_message INT;
+    SET return_message = 2;
+    
+	IF EXISTS (SELECT pwd.usr_secret FROM pwd WHERE pwd.FK_usr_ID = whoAmI AND pwd.usr_secret = old_pwd)
+    THEN
+		SET return_message = 3;
+		IF (new_pwd = new_pwd_check)
+        THEN
+			UPDATE pwd SET usr_secret = new_pwd WHERE pwd.FK_usr_ID = whoAmI AND pwd.usr_secret = old_pwd;
+            SET return_message = 1;
+		END IF;
+    END IF;
+    
+	RETURN return_message;
+END// DELIMITER ;
+
+
+
+-- USE grp_pro; 
