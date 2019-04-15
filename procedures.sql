33a8e28f4369abd31df8e4a53b1bf48f47d3e7da
Piggilinux <37959674+Piggilinux@users.noreply.github.com>
2019-04-15 10:14:24 -0500

    Add files via upload

--- procedures.sql
+++ procedures.sql
@@ -133,5 +133,20 @@ BEGIN
 END// DELIMITER ;
 
 
+# A list of users friends will be shown
+DROP PROCEDURE IF EXISTS view_friends;
+DELIMITER //
+CREATE PROCEDURE view_friends (whoAmI VARCHAR(255))
+  BEGIN
+	SELECT CONCAT(usr_firstname, " ",usr_lastname) AS Friends 
+    FROM users 
+    WHERE usr_ID IN 
+		(SELECT REPLACE(CONCAT(usr_1, usr_2), whoAmI, '') AS Friends 
+		FROM usr_relations 
+        WHERE usr_1 = whoAmI OR usr_2 = whoAmI);
+  END //
+DELIMITER ;
+
+
 
 -- USE grp_pro; 
