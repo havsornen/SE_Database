2234e722a5b33ab8b0929dd7c4341e75dd8bb2b6
Piggilinux <37959674+Piggilinux@users.noreply.github.com>
2019-05-02 15:10:27 +0200

    Add files via upload

--- procedures.sql
+++ procedures.sql
@@ -216,7 +216,17 @@ CREATE PROCEDURE get_friend_mail (whoAmI VARCHAR(255))
 DELIMITER ;
 
 
-
+# put usr_ID in parameter and both non accepted and accepted events will be displayed.
+DROP PROCEDURE IF EXISTS get_activities;
+DELIMITER //
+CREATE PROCEDURE get_activities (whoAmI VARCHAR(255))
+  BEGIN
+    SELECT * FROM activity ac 
+    WHERE ac.activity_ID IN 
+		(SELECT ai.activity_ID FROM activity_invitees ai 
+		WHERE usr_ID = whoAmI);
+  END //
+DELIMITER ;
 
 
 
